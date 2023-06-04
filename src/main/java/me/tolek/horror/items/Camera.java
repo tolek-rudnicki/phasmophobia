ad950cbd07ee0511ec9456b2768e00916385371a
Tolek Rudnicki <tolek@epsi.pl>
2023-06-05 00:22:28 +0200

    fc

--- src/main/java/me/tolek/horror/items/Camera.java
+++ src/main/java/me/tolek/horror/items/Camera.java
@@ -1,5 +1,6 @@
 package me.tolek.horror.items;
 
+import me.tolek.horror.Utils;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
@@ -15,7 +16,15 @@ public class Camera implements Listener {
         Location eLoc = e1.getLocation();
         Location pLoc = p.getLocation();
 
-        
+        int o = pLoc.getBlockX() - eLoc.getBlockX();
+        int a = pLoc.getBlockZ() - eLoc.getBlockZ();
+
+        double yaw = Math.tan(o/a);
+
+        if (pLoc.getYaw() + 60 < yaw && pLoc.getYaw() - 60 < yaw) {
+            return true;
+        }
+
         return false;
     }
 
@@ -30,7 +39,8 @@ public class Camera implements Listener {
 
 
         if (name.equalsIgnoreCase("Camera")) {
-
+            p.sendMessage("EIP: " + entityInPath(Utils.e, p));
+            event.setCancelled(true);
         }
     }
 
